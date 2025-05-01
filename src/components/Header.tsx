ebff0ffab7c9587e95c57d31cf646113a739eac6
Pol <zhperon@gmail.com>
2025-04-30 18:47:35 -0700

    fixed up the design to make it more interesing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router";
-import BurgerLink from "./BurgerLink";
+import BurgerLink from "../assets/burger-logo.png";
 import { Wrench, Mail, FileUser } from "lucide-react";
 const Header = ()=>{
 
     return (
         <>
         <div id="header-full">
-        <Link to="/" id='burgerLink'><BurgerLink/></Link>
-        <div id='header' className="font-jakarta flex justify-evenly w-full items-center mx-auto text-2xl">
+        <Link to="/" id='burgerLink'><img src={BurgerLink}></img></Link>
+        <div id='header' className="font-jakarta flex justify-evenly w-full items-center text-2xl">
             <Link className = 'linkClass flex items-center' to="/projects"><div className="names">projects</div><div className="icons"><Wrench/></div></Link>
             <Link className = 'linkClass flex items-center' to="/resume"><div className="names">resume</div><div className="icons"><FileUser/></div></Link>
             <Link className = 'linkClass flex items-center' to="/contact"><div className="names">contact</div><div className="icons"><Mail/></div></Link>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,28 +5,30 @@ import GH from "../assets/github-mark-white.png"
 const Home = ()=>{
     return (
         <>
+        
         <div className="responsiveDiv">
             <div id='sweDevAni' className="relative max-w-7xl mx-auto px-16 py-64 flex">
-                <div>
-                <h1 id='myName' className="font-monoton text-5xl">Polina Mejia</h1>
-                <h2 className="font-jakarta text-teal-300 text-5xl">&gt;_ junior swe <br></br>+ web developer</h2>
-                <div className="flex my-12">
-                <a href='https://www.linkedin.com/in/polina-mejia-432080284/' target="_blank">
-                    <img className="social" src={LinkedIn} width={80} ></img>
-                </a>
-                <a href='https://github.com/Poleron402' target="_blank">
-                    <img src={GH}  width={80} className="social ml-24"></img>
-                </a>
-                </div>
+                <div id="leftAlignedText">
+                    <h1 id='myName' className="font-monoton text-5xl">Polina Mejia</h1>
+                    <h2 className="font-jakarta text-teal-300 text-5xl">&gt;_ junior swe <br></br>+ web developer</h2>
+                    <div className="flex my-12">
+                    <a href='https://www.linkedin.com/in/polina-mejia-432080284/' target="_blank">
+                        <img className="social" src={LinkedIn} width={80} ></img>
+                    </a>
+                    <a href='https://github.com/Poleron402' target="_blank">
+                        <img src={GH}  width={80} className="social ml-24"></img>
+                    </a>
+                    </div>
                 
                 </div>
             </div>
             <div id='burgerDiv'>
                 <Burger/>
             </div>
-
+            
         </div>
         <Outlet />
+
         </>
     )
 }
