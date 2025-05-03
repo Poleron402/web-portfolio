f146e913cd9446ca998ebfcc4154f6ed24b7c429
Pol <zhperon@gmail.com>
2025-05-02 19:08:48 -0700

    'lil' Terminal done

--- src/components/MyTerminal.tsx
+++ src/components/MyTerminal.tsx
@@ -11,26 +11,54 @@ const MyTerminal: React.FC<IndividualSchoolProjectProps> = ({project})=>{
     const [command, setCommand] = useState<string>("")
     const [projects, setProjects] = useState<ProjectList[]>(project.projects)
     const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) =>{
-        let commandArr = command.split(" ")
+        let [myCommand, ...argument] = command.split(" ")
+        let strArgument = argument.join(' ')
         if (e.key === 'Enter'){
-            switch (commandArr[0]){
+            switch (myCommand){
                 case "clear":
                     setHistory([])
                     setCommand("")
                     break
                 case "help":
                     setHistory((prevItems)=> [...prevItems, {command: command,
-                         result: "=== Available commands ===<br>ls - list projects<br>cat [PROJECT] - read what the project is about<br>gh [PROJECT] - open GitHub of the project in a new tab<br>"}])
+                         result: "=== Available commands ===<br>ls - list projects<br>cat [PROJECT] - read what the project is about<br>links [PROJECT] - get the links for the project <br>"}])
                          setCommand("")
                          break;
                 case "ls":
                     let returnString:string = ""
-                    console.log(projects)
+
                     for (let proj of projects){
                         returnString += proj.title+" "
                     }
                     setHistory((prevItems)=> [...prevItems, {command: command,
                         result: returnString}])
+                        setCommand("")
+                        break;
+                case "cat":
+                    let descString:string = ""
+
+                    for (let proj of projects){
+                        if (strArgument === proj.title){
+                            descString += proj.description+" "
+                        }
+                        
+                    }
+                    setHistory((prevItems)=> [...prevItems, {command: command,
+                        result: descString}])
+                        setCommand("")
+                        break;
+                case "links":
+                    let linksString:string = ""
+                    for (let proj of projects){
+                        if (strArgument === proj.title){
+                            for (let link of proj.links){
+                                linksString+=`<a class="terminalLinks" target="_blank" href=${link.link}>➜ ${proj.title}_${link.type}</a>&nbsp;&nbsp;`
+                            }
+                            
+                        }
+                    }
+                    setHistory((prevItems)=> [...prevItems, {command: command,
+                        result: linksString}])
                         setCommand("")
                         break;
                 default:
@@ -50,7 +78,7 @@ const MyTerminal: React.FC<IndividualSchoolProjectProps> = ({project})=>{
             
             <div id="comments">
                 <p>
-                    **** Of course I will code up a web Terminal for OS Class :) ****
+                    **** Of course I will code up a web Terminal for OS Class 🤖 ****
                     <br></br>Type 'help' to see the list of available commands
                 </p>
             </div>
@@ -63,7 +91,7 @@ const MyTerminal: React.FC<IndividualSchoolProjectProps> = ({project})=>{
                 </>
             ))}
             <div className="inLine">
-                <p>🌸 pol@polsportfolio $ &nbsp;</p> 
+                <p>🌸 pol@polsportfolio  $ &nbsp;</p> 
                 <input autoComplete="off" autoFocus type="text" value = {command} onKeyDown={e=>handleKeyDown(e)} id="termInput" onChange={e=>setCommand(e.target.value)} ></input>
             </div>
         </div>
